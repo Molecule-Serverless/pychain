ebc618788017068a1aa2c1234cdcddd9aeb320c9
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-12 20:13:40 +0800

    scripts refine: eliminate unnecessary error msgs in delete_containers
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- delete_containers.sh
+++ delete_containers.sh
@@ -3,12 +3,14 @@ source config
 # sudo runc delete -f $BASELINE_DRIVER_CONTAINER_ID
 # sudo runc delete -f $BASELINE_MAPPER_CONTAINER_ID
 # sudo runc delete -f $BASELINE_REDUCER_CONTAINER_ID
-docker stop $BASELINE_DRIVER_CONTAINER_ID
-docker stop $BASELINE_MAPPER_CONTAINER_ID
-docker stop $BASELINE_REDUCER_CONTAINER_ID
+docker stop $BASELINE_DRIVER_CONTAINER_ID > /dev/null 2>&1
+docker stop $BASELINE_MAPPER_CONTAINER_ID > /dev/null 2>&1
+docker stop $BASELINE_REDUCER_CONTAINER_ID > /dev/null 2>&1
 
-docker stop $MOLECULEIPC_DRIVER_CONTAINER_ID
-docker stop $MOLECULEIPC_MAPPER_CONTAINER_ID
-docker stop $MOLECULEIPC_REDUCER_CONTAINER_ID
+docker stop $MOLECULEIPC_DRIVER_CONTAINER_ID > /dev/null 2>&1
+docker stop $MOLECULEIPC_MAPPER_CONTAINER_ID > /dev/null 2>&1
+docker stop $MOLECULEIPC_REDUCER_CONTAINER_ID > /dev/null 2>&1
 
-docker rm $(docker ps -aq)
+docker rm $(docker ps -aq) > /dev/null 2>&1
+
+echo "Clean successfully!"
